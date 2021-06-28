b051f835c0e82f8ec030f9a5090a38df6de16150
FAUX Manuel <Manuel.FAUX@frequentis.com>
2021-06-28 23:25:04 +0200

    Arrange Sublime preview in side-by-side group

--- diagram/sublime3.py
+++ diagram/sublime3.py
@@ -13,7 +13,17 @@ class Sublime3Viewer(BaseViewer):
 		if not (sublime.version().startswith('3') or sublime.version().startswith('4')):
 			raise Exception("Not Sublime 3 or 4!")
 
-	def view(self,diagram_files):
+	def view(self, diagram_files):
 		for diagram_file in diagram_files:
 			if diagram_file:
-				sublime.active_window().open_file(diagram_file.name)
+				window = sublime.active_window()
+				if window.num_groups() < 2:
+					window.set_layout({
+						"cols": [0.0, 0.5, 1.0],
+						"rows": [0.0, 1.0],
+						"cells": [[0, 0, 1, 1], [1, 0, 2, 1]]
+						})
+				
+				window.focus_group(1)
+				window.open_file(diagram_file.name)
+				window.focus_group(0)
